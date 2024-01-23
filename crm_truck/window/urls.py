f7f054317a52b83e190982c4d469a3af7779cb78
mattia allegro <mattia.allegro@yahoo.it>
2024-01-23 08:49:27 +0100

    added draft urls

--- crm_truck/window/urls.py
+++ crm_truck/window/urls.py
@@ -7,5 +7,15 @@ urlpatterns = [
         "",
         Index.as_view(),
         name="home-index",
+    ),
+    path(
+        "contact-us/",
+        Index.as_view(),
+        name="contact-us",
+    ),
+    path(
+        "blog/",
+        Index.as_view(),
+        name="blog",
     )
 ]
